557cfcc13ea3cdf7d96b05a05af6fa3a4cbfedee
Clerc Steven <clerc.steven@gmail.com>
2022-11-16 17:04:20 +0100

    chore(global): add configs

--- histoire.config.js
+++ histoire.config.js
@@ -1,8 +1,27 @@
-import { defineConfig } from 'histoire'
+import { defineConfig, defaultColors } from 'histoire'
 import { HstVue } from '@histoire/plugin-vue'
 
 export default defineConfig({
   plugins: [
     HstVue(),
   ],
+  tree: {
+    file: 'title',
+    order: 'asc',
+  },
+  theme: {
+    title: 'Pumpkin Vue',
+    favicon: '/pumpkin.png',
+    logo: {
+      square: '/src/img/pumpkin.png',
+      light: '/src/img/pumpkin.png',
+      dark: '/src/img/pumpkin.png',
+    },
+    colors: {
+      primary: defaultColors.orange,
+      gray: defaultColors.green,
+    },
+    logoHref: '#',
+  },
+  autoApplyContrastColor: true,
 })
